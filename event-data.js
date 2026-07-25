0cbb0ac608eb4c10e9f46fb651adf3ba3f9078f6
albaijiuser <157370391+albaijiuser@users.noreply.github.com>
2026-07-25 20:53:20 +0300

    Create event-data.js

---
+++ event-data.js
@@ -0,0 +1,25 @@
+const even =[
+  {
+    id: "sirius-workshop",
+    title: "Sirius Workshop",
+    date: "2026-08-01",
+    location: "Robotics Lab 2",
+    description: " A beginner-friendly workshop where students build thier first simple robot using basic sensors and motors."
+  },
+
+  {
+    id: "antares-competition",
+    title: "Antares Competition",
+    date: "2026-08-05",
+    location: "...",
+    description: "...",
+  },
+
+  {
+    id: "elphant-mountain-trip",
+    title: "Elephant Mountain Field",
+    date: "2026-08-10",
+    location: "...",
+    description: "...",
+  }
+];  
