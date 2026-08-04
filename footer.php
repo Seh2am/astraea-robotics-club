50947f6e8ee4ed9d9fe10f82a8bbd93c6a4a6a30
Seh2am <seham.ui@gmail.com>
2026-08-04 14:10:50 +0300

    Add Shared header and footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+</main>
+<footer>
+<p>&copy; 2026 Astraea Robotics Club - All rights reserved.</p>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Astraea Robotics Club</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+<img src="logo.svg" alt="Astraea Robotics Club Logo" class="site-logo">
+<h1>Astraea Robotics Club</h1>
+<nav>
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="events.php">Events</a></li>
+<li><a href="registration.php">Register</a></li>
+<li><a href="registrations-list.php">Registrations</a></li>
+<li><a href="about.php">About</a></li>
+</ul>
+</nav>
+</header>
+<main>
